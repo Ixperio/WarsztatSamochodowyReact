612352627d0294950b5d8a1af08a6ba6efb90dd4
CinekTV <cinekv15@gmail.com>
2024-01-27 19:52:33 +0100

    postęp +

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
@@ -6,6 +6,8 @@ import Details from './pages/Details'
 import NotFound from './pages/NotFound'
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Home from './components/Home';
+import List from './pages/List';
 
 function App() {
 
@@ -13,6 +15,9 @@ function App() {
   <>
     <Header/>
     <Routes>
+      <Route path='/list' element={<List/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/home' element={<Home/>}/>
       <Route path='/details' element={<Details/>}/>
       <Route path='/details/:id' element={<Details/>}/>
       <Route path='*' element={<NotFound/>}/>
@@ -24,4 +29,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () =>{
+    return(
+        <h1>Witaj na stronie Warsztatu Samochodowego!</h1>
+    )
+}
+
+export default Home;
