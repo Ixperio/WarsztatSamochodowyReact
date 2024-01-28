65ee63630aa8f2e2404e23c79283e1161680a5ae
CinekTV <cinekv15@gmail.com>
2024-01-28 15:32:24 +0100

    po mergu z updateowaniem i bugami

--- src/components/ButtonComponent/UpdateButton.tsx
+++ src/components/ButtonComponent/UpdateButton.tsx
@@ -1,12 +1,49 @@
+import apiService from "../../services/apiService";
+
 interface ButtonProps{
     // children: React.ReactNode;
     value: string;
     children: string;
+    data: string;
 }
 
 const Button: React.FC<ButtonProps> = (props) =>{
+    let x: number = 0;
+    switch (props.value){
+        case "UpdateName":
+            console.log(props.data)
+            x = 1
+            break;
+        case "UpdateSurname":
+            x = 2
+            break;
+        case "UpdatePhone":
+            x = 3
+            break;
+        case "UpdateAddress":
+            x = 4
+            break;
+         case "UpdateCity":
+            x = 5
+            break;
+        case "UpdatePost":
+            x = 6
+            break;
+        default:
+            console.error("Value not declared")
+            break;
+    }
     const handleClick = () => {
-        console.log("Update ", props.children)
+        switch (x) {
+            case 1:
+                apiService.updateName(props.data)
+                break;
+        
+            default:
+                console.error("Updating index wrong!")
+                break;
+        }
+        console.log("Update")
     };
     return(
         <button type="button"  className="update_button" onClick={handleClick}>{props.children}</button>
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -2,6 +2,7 @@ import apiService from "../services/apiService"
 import DisplayData from "../components/DisplayDataComponent/DisplayData"
 import UpdateButton from "../components/ButtonComponent/UpdateButton"
 import Button from "../components/ButtonComponent/Button"
+import { useState } from "react"
 
 
 interface UserData{
@@ -23,28 +24,35 @@ const testiong = async () =>{
 
 await testiong();
 
-const Edit = () =>{
+const Edit: React.FC = () =>{
+    const [inputValue, setInputValue] = useState(userdata.name);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(event.target.value);
+    };
+    
+
     return(
         <div>
             <div className="user_data">
                 <DisplayData value={""}>Imię:</DisplayData>
                 <DisplayData value={""}>Nazwisko:</DisplayData>
-                <input type="text" name="name" className="update_input" value={userdata.name}/>
+                <input type="text" name="name" className="update_input" value={inputValue} onChange={handleInputChange}/>
                 <input type="text" name="surname" className="update_input" value={userdata.surname}/>
-                <UpdateButton value="/UpdateName">Aktualizuj</UpdateButton>
-                <UpdateButton value="/UpdateSurname">Aktualizuj</UpdateButton>
+                <UpdateButton data={inputValue} value="UpdateName">Aktualizuj</UpdateButton>
+                <UpdateButton data={userdata.surname} value="UpdateSurname">Aktualizuj</UpdateButton>
                 <DisplayData value={""}>Nr. Telefonu:</DisplayData>
                 <DisplayData value={""}>Adres:</DisplayData>
                 <input type="text" name="phone" className="update_input" value={userdata.phone}/>
                 <input type="text" name="address" className="update_input" value={userdata.adress}/>
-                <UpdateButton value="/UpdatePhone">Aktualizuj</UpdateButton>
-                <UpdateButton value="/UpdateAdress">Aktualizuj</UpdateButton>
+                <UpdateButton data={userdata.phone} value="UpdatePhone">Aktualizuj</UpdateButton>
+                <UpdateButton data={userdata.adress} value="UpdateAdress">Aktualizuj</UpdateButton>
                 <DisplayData value={""}>Miasto:</DisplayData>
                 <DisplayData value={""}>Kod Pocztowy:</DisplayData>
                 <input type="text" name="city" className="update_input" value={userdata.city}/>
                 <input type="text" name="postcode" className="update_input" value={userdata.postCode}/>
-                <UpdateButton value="/UpdatCity">Aktualizuj</UpdateButton>
-                <UpdateButton value="/UpdatePost">Aktualizuj</UpdateButton>
+                <UpdateButton data={userdata.city} value="UpdatCity">Aktualizuj</UpdateButton>
+                <UpdateButton data={userdata.postCode} value="UpdatePost">Aktualizuj</UpdateButton>
                 <DisplayData value={userdata.birthday}>Data urodzin:</DisplayData>
                 <DisplayData value={userdata.email}>Email:</DisplayData>
             </div>
